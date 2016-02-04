b00078c9c8610a5708bd3504bbba127ae003c9f2
Thomas Tucker <elteano@gmail.com>
2016-02-04 10:50:17 -0800

    Completed main lab body.

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,12 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+exports.addFriend = function(req, res) {
+	var newobj = {
+		'name': req.query.name,
+		'description': req.query.description,
+		'imageURL': 'http://lorempixel.com/400/400/people'
+	};
+	console.log(newobj);
+	data['friends'].push(newobj);
+	res.render('add');
+}
